b70e153b64692b15f63c6ba37014b74cb021a1f2
Dzmitry.Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2019-11-28 13:40:42 +0300

    refactor: replace code from app into routes file

--- app.js
+++ app.js
@@ -1,46 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes.js');
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    // process.exit(1);
-    const { url, method, headers } = req;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write(
-            '<body><form action="/message" method="POST"><input name="message" type="text><button type="submit">Send</button></form></body>'
-        );
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-
-        req.on('data', chunk => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            fs.writeFile('message.txt', `dummy: ${parsedBody}`, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
