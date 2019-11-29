3aa0e83f1e8b77e99f1c381d622a4f44c7911c04
Dzmitry.Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2019-11-29 16:04:05 +0300

    refactor: rewrite logic with express router object

--- app.js
+++ app.js
@@ -2,28 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
 /**
  * When extended property is set to true,
  *  the URL-encoded data will be parsed with the qs library.
  */
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/add-product', (req, res, next) => {
-    res.send(`
-    <form action="/product" method="post">
-        <input type="text" name="title">
-            <button type="submit">Add product</button>
-        </input>
-    </form>`);
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h2>I feel good<h2>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
