a99928a93fff795a1fa93ace86d68327af3b7309
Dzmitry.Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2019-11-29 12:37:22 +0300

    feat: implement response by send method

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log('In the next middleware!');
+    res.send('<h2>Hello from Express<h2>');
 });
 
 const server = http.createServer(app);
