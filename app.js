76f2bc9ee21c04846bf79932dd3d417f64b30931
Dzmitry.Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2019-11-29 12:31:25 +0300

    feat: implement two use middleware functions

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const http = require('http');
-const routes = require('./routes.js');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes);
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
 
+app.use((req, res, next) => {
+    console.log('In the next middleware!');
+});
+
+const server = http.createServer(app);
+// const router = require('./router.js');
+// const server = http.createServer(router);
 server.listen(3000);
