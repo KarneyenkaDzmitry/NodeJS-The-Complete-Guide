4c56f3464ae275afd5fe8673c14b32f53eb23439
Dzmitry.Karneyenka <Dzmitry.Karneyenka@wolterskluwer.com>
2019-11-29 12:54:17 +0300

    feat: play around path arg with use function

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use('/', (req, res, next) => {
+    console.log('It is always executed!');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/hello', (req, res, next) => {
     console.log('In the next middleware!');
     res.send('<h2>Hello from Express<h2>');
+});
+
+app.use('/health', (req, res, next) => {
+    console.log('In the health middleware!');
+    res.send('<h2>I feel good<h2>');
 });
 
 app.listen(3000);
